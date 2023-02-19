9a1de312e4903ba91d73f3605e4efbd5c3fcc812
Dany Nguyen <danielnguyen@outlook.fr>
2023-02-19 22:51:46 +0100

    Update index.js
    Change hero banner for test

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Première collection Montaigne'}
+        subtitle={'Créée à la main, avec amour et Nocciola'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
